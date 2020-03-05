cf4e6efc1a684d4e9af85e2a7c4d8a6e4ea97364
hilldmc <hilldmc@gmail.com>
2020-03-05 10:58:21 -0800

    ajax now returning author, link and quote but have to refine

--- js/api.js
+++ js/api.js
@@ -16,9 +16,21 @@
 
       .done(function (data) {
         console.log(data);
-      });
+
+        data.forEach(element => {
+          let ranQuote = (`
+          <p class='quote-content'>${element.content.rendered}</p>
+          <p class='quote-author'>${element.title.rendered}</p>
+          <a href="${element._qod_quote_source_url}" target="_blank">
+            <p class='quote-source'>${element._qod_quote_source}</p>
+          </a>
+          `);
+          anotherQuote.append(ranQuote);
+        });
+
+      }); // end of .done()
 
 
-  }); // end of .done()
+  }); // end of event listener
 
 })(jQuery); // end of DOM Content Load
