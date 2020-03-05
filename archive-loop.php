ec8228271b1b565986f8764d7ca5b8a608497a3f
hilldmc <hilldmc@gmail.com>
2020-03-04 19:49:21 -0800

    headings added

--- archive-loop.php
+++ archive-loop.php
@@ -18,43 +18,46 @@ get_header(); ?>
 				?>
       </header><!-- .page-header -->
 
-      <?php
-        $args = array( 
-          'post_type' => 'post',
-          'posts_per_page'	=> -1
-        );
-        $blog_posts = get_posts( $args ); 
-        $categories =  get_categories( $args);
-      ?>
-      
-      <div class="author-loop" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-      <?php foreach($blog_posts as $post): setup_postdata ($post); ?>
-        <a href="<?php the_permalink()?>">
-						<h2><?php the_title() ?></h2>
-              </a>
-                   
-			<?php endforeach; ?>
-			<?php  wp_reset_postdata(); ?>
-      </div> 	<!-- journal-grid -->
-
-      <div class="category-loop" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-      <?php foreach($categories as $category): setup_postdata ($category); ?>
-      <a class="button-text" href="<?php echo get_category_link($category); ?>">
-        <li><?php echo $category->cat_name ?></li>
-              </a>
-                   
-			<?php endforeach; ?>
-			<?php  wp_reset_postdata(); ?>
-      </div> 	<!-- journal-grid -->
-      
-
-
-      <ul>
-        <?php 
-        $wptc = wp_tag_cloud('smallest=12&largest=12&orderby=count&order=DESC&format=array&unit=px&echo=0'); 
-        foreach( $wptc as $wpt ) echo "<li>" . $wpt . "</li>\n"; 
+        <?php
+          $args = array( 
+            'post_type' => 'post',
+            'posts_per_page'	=> -1
+          );
+          $blog_posts = get_posts( $args ); 
+          $categories =  get_categories( $args);
         ?>
-      </ul>
+        
+          <div class="author-loop" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+          <h2>Quote Authors</h2>
+            <?php foreach($blog_posts as $post): setup_postdata ($post); ?>
+              <a href="<?php the_permalink()?>">
+                <p><?php the_title() ?></p>
+              </a>
+                   
+            <?php endforeach; ?>
+            <?php  wp_reset_postdata(); ?>
+          </div> 	<!-- author-loop -->
+
+          <div class="category-loop" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+          <h2>Categories</h2>
+            <?php foreach($categories as $category): setup_postdata ($category); ?>
+              <a  href="<?php get_category_link($category); ?>">
+                <p><?php echo $category->cat_name ?></p>
+              </a>
+                   
+            <?php endforeach; ?>
+            <?php  wp_reset_postdata(); ?>
+           </div> 	<!-- category-loop -->
+      
+
+
+          <div class="tag-loop">
+          <h2>Tags</h2>
+            <?php 
+            $wptc = wp_tag_cloud('smallest=12&largest=12&orderby=count&order=DESC&format=array&echo=0'); 
+            foreach( $wptc as $wpt ) echo "<p>" . $wpt . "</p>\n"; 
+            ?>
+          </div> <!-- tag-loop -->
 
 
 
