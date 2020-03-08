2f3655f191f168e04802415a104cc27124c1960a
hilldmc <hilldmc@gmail.com>
2020-03-07 18:19:00 -0800

    home page meta links correctly and styled

--- template-parts/content.php
+++ template-parts/content.php
@@ -16,8 +16,17 @@
 			<?php 
 			$meta_quote_src = get_post_meta( get_the_ID(), '_qod_quote_source', true );
 			$meta_quote_src_url = get_post_meta( get_the_ID(), '_qod_quote_source_url', true ); ?>
+			
+			<?php if (strlen($meta_quote_src_url) > 0) : ?>
+			<p class='quote-author'> — <?php the_title(); ?>, <span class='quote-src-link'><a href="<?php echo $meta_quote_src_url ?>" target="_blank"><?php echo $meta_quote_src ?></a></span></p>
 
-			<p class='quote-author'> — <?php the_title(); ?><span class='quote-src-link'><a href="<?php echo $meta_quote_src_url ?>" target="_blank"><?php echo $meta_quote_src ?></a></span></p>
+			<?php elseif (strlen($meta_quote_src) > 0) : ?>
+				<p class='quote-author'> — <?php the_title(); ?>, <span class='quote-src-link'><?php echo $meta_quote_src ?></span></p>
+			
+			<?php else : ?>
+				<p class='quote-author'> — <?php the_title(); ?> <span class='quote-src-link'><?php echo $meta_quote_src ?></span></p>
+
+			<?php endif; ?> 
 			
 		</div>
 	</div><!-- .entry-content -->
